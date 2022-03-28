a4e745187b6be70d8658f9b0b2c33a91b5212e1e
Alberto Fernandez <infjaf@gmail.com>
2022-03-28 15:47:05 +0200

    Next release cycle
    Signed-off-by: Alberto Fernandez <infjaf@gmail.com>

--- core/src/test/integration/org/richfaces/deployment/CoreDeployment.java
+++ core/src/test/integration/org/richfaces/deployment/CoreDeployment.java
@@ -447,7 +447,7 @@ public class CoreDeployment extends BaseDeployment {
     }
     
     public void withA4jComponents() {
-        addMavenDependency("com.github.albfernandez.richfaces:richfaces-a4j:4.6.18.ayg");
+        addMavenDependency("com.github.albfernandez.richfaces:richfaces-a4j:4.6.19-SNAPSHOT");
         excludeMavenDependency("richfaces-core");
     }
     

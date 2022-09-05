81cddbe842f31892453cd605e77f1a16fb526170
Alberto Fernandez <infjaf@gmail.com>
2022-09-05 11:13:40 +0200

    Bump jsoup from 1.14.2 to 1.15.3

--- examples/showcase/src/main/java/org/richfaces/demo/validation/SafeHtmlValidator.java
+++ examples/showcase/src/main/java/org/richfaces/demo/validation/SafeHtmlValidator.java
@@ -8,17 +8,17 @@ import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
 import org.jsoup.Jsoup;
-import org.jsoup.safety.Whitelist;
+import org.jsoup.safety.Safelist;
 
 @FacesValidator("safeHtml")
 public class SafeHtmlValidator implements Validator {
 
-    private static Whitelist whitelist = Whitelist.simpleText();
+    private static Safelist safelist = Safelist.simpleText();
 
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         if (value != null) {
-            if (!Jsoup.isValid(value.toString(), whitelist)) {
+            if (!Jsoup.isValid(value.toString(), safelist)) {
                 FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_WARN, "message contains unsafe character sequence", "");
                 throw new ValidatorException(message);
             }

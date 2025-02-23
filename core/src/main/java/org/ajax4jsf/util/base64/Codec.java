79b2b7915888f00b4641854116100058eec33113
MilovdZee <milo@vanderzee.org>
2025-02-23 13:59:00 +0100

    Always encrypt in Codec

--- core/src/main/java/org/ajax4jsf/util/base64/Codec.java
+++ core/src/main/java/org/ajax4jsf/util/base64/Codec.java
@@ -20,6 +20,7 @@
  */
 package org.ajax4jsf.util.base64;
 
+import com.google.common.primitives.Bytes;
 import jakarta.faces.FacesException;
 
 import javax.crypto.Cipher;
@@ -28,13 +29,15 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
+import java.util.Arrays;
 
 public class Codec {
     private static final String ALGORITHM = "AES/CFB/PKCS5Padding";
     private static final int AES_KEY_SIZE = 128; // in bits
+    private static final int IV_LENGTH = 16;
 
-    private final Cipher decripter;
-    private final Cipher encripter;
+    private final SecureRandom random;
+    private final SecretKey secretKey;
 
     public Codec() {
         super();
@@ -42,23 +45,10 @@ public class Codec {
         // Initialize the Ciphers
         try {
             // Create a random secret key
-            SecureRandom random = SecureRandom.getInstanceStrong();
+            random = SecureRandom.getInstanceStrong();
             KeyGenerator keyGen = KeyGenerator.getInstance("AES");
             keyGen.init(AES_KEY_SIZE, random);
-            SecretKey secretKey = keyGen.generateKey();
-
-            // Create the initialization vector
-            byte[] iv = new byte[16];
-            new SecureRandom().nextBytes(iv);
-            IvParameterSpec spec = new IvParameterSpec(iv);
-
-            // Init the encripter
-            encripter = Cipher.getInstance(ALGORITHM);
-            encripter.init(Cipher.ENCRYPT_MODE, secretKey, spec, random);
-
-            // Init the decripter
-            decripter = Cipher.getInstance(ALGORITHM);
-            decripter.init(Cipher.DECRYPT_MODE, secretKey, spec, random);
+            secretKey = keyGen.generateKey();
         } catch (Exception e) {
             throw new FacesException("Error set encryption key", e);
         }
@@ -76,13 +66,33 @@ public class Codec {
         return new String(utf8, StandardCharsets.UTF_8);
     }
 
-    public synchronized byte[] decode(byte[] src) throws Exception {
-        byte[] dec = URL64Codec.decodeBase64(src);
-        return decripter.doFinal(dec);
+    public byte[] decode(byte[] src) throws Exception {
+        byte[] bytes = URL64Codec.decodeBase64(src);
+
+        // First IV_LENGTH bytes contain the IV
+        byte[] iv = Arrays.copyOfRange(bytes, 0, IV_LENGTH);
+        IvParameterSpec spec = new IvParameterSpec(iv);
+
+        // Init the decripter
+        Cipher decripter = Cipher.getInstance(ALGORITHM);
+        decripter.init(Cipher.DECRYPT_MODE, secretKey, spec, random);
+
+        byte[] encrypted = Arrays.copyOfRange(bytes, IV_LENGTH, bytes.length);
+        return decripter.doFinal(encrypted);
     }
 
-    public synchronized byte[] encode(byte[] src) throws Exception {
-        byte[] dec = encripter.doFinal(src);
-        return URL64Codec.encodeBase64(dec);
+    public byte[] encode(byte[] src) throws Exception {
+        // Create the initialization vector
+        byte[] iv = new byte[IV_LENGTH];
+        new SecureRandom().nextBytes(iv);
+        IvParameterSpec spec = new IvParameterSpec(iv);
+
+        // Init the encripter
+        Cipher encripter = Cipher.getInstance(ALGORITHM);
+        encripter.init(Cipher.ENCRYPT_MODE, secretKey, spec, random);
+
+        byte[] encrypted = encripter.doFinal(src);
+        byte[] bytes = Bytes.concat(iv, encrypted);
+        return URL64Codec.encodeBase64(bytes);
     }
 }
